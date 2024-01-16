1da8ee047cf89c287ddd2c219902a9e35b78fc80
Keven Christian <113730071+01Keven@users.noreply.github.com>
2024-01-16 18:12:14 -0300

    :construction: Section New

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,10 +27,10 @@ const linkAction = () => {
 navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 // Add Blur Header
-const scrollHeader = () => {
-  const header = document.getElementById("header");
+const blurHeader = () => {
+  const header = document.getElementById("header"); 
   this.scrollY >= 50
-    ? header.classList.add("scroll-header")
-    : header.classList.remove("scroll-header");
+    ? header.classList.add("blur-header")
+    : header.classList.remove("blur-header");
 };
-window.addEventListener("scroll", scrollHeader);
+window.addEventListener("scroll", blurHeader);
